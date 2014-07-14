2a892759bb51a6b76c5efa123a64cce7fadefc07
Lakshmi Ashok <lakshmiashok@Lakshmi-Ashoks-MacBook-Air.local>
2014-07-14 12:12:30 +0530

    Initial Commit

---
+++ Keyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Keyboard
+//
+//  Created by Lakshmi Ashok on 14/07/14.
+//  Copyright (c) 2014 Invention. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
